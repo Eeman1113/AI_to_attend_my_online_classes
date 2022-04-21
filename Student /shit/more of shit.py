580fca46cbc870855842db74b943f80319107670
Eeman Majumder <54275491+Eeman1113@users.noreply.github.com>
2022-04-21 18:14:12 +0530

    Add files via upload

---
+++ Student /shit/more of shit.py
@@ -0,0 +1,65 @@
+import string
+
+
+def convert(lst):
+  return (lst[0].split())
+
+def line_maker():        
+    lines=[]
+    file = open("/Users/eemanmajumder/code_shit/abstract/Student /text/testing1.txt", "w+")
+    with open ('/Users/eemanmajumder/code_shit/abstract/Student /text/recognized.txt', "r") as f:
+      lines = [line.rstrip() for line in f]
+    
+    for i in lines:
+      if i=='':
+        lines.remove(i)
+    file.write(str(lines))
+    file.close()
+    
+    return lines
+
+def extractDigits(lst):
+    return [[el] for el in lst]
+                
+
+file = open("/Users/eemanmajumder/code_shit/abstract/Student /text/recognized.txt", "r")
+j=file.read()
+k=[]
+k.append(j)
+l=convert(k)
+#print(l)
+print(line_maker())
+r=line_maker()
+q=extractDigits(r)
+r=len(q)
+m=0
+for i in range(0,r):
+  m=q[i]
+  q.append(convert(m))
+file = open("/Users/eemanmajumder/code_shit/abstract/Student /text/tokenised.txt", "w+")
+print(q)
+file.write(str(q))
+x=0
+y=0
+for i in range(0,len(q)-1):
+  x=len(q[i])
+  if i==len(q)-1:
+    break
+  else:
+    y=len(q[i+1])
+    
+  if  x>1 and y>1:
+     if q[i][0]==q[i+1][0] and q[i][1]==q[i+1][1]:
+       
+       if x>y:
+         q.remove(q[i+1])
+       else:
+         q.remove(q[i])
+   
+     else: 
+       continue
+  else:
+    continue
+      
+print(q)
+   
